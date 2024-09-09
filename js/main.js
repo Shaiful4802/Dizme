04d884db9834cfffc99fe51bb66a11698e77661d
Shaiful4802 <office.shaiful@gmail.com>
2024-09-09 14:05:46 +0600

    portfulio section updat

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+  "use strict"
+
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1500,
+    offset: 100,
+    beginAt: 0,
+    });
+
+    // CommonJS
+  var mixer = mixitup(".box-list");
+  
+})
